bbd5743252ff47bfebdb13fe464f4a5f984a978c
Alphonso77 <762224+alphonso77@users.noreply.github.com>
2025-06-04 10:29:19 -0400

    add sendCsv & sendCsvFromObjectArray

--- src/slack-service.ts
+++ src/slack-service.ts
@@ -1,5 +1,8 @@
 import { WebClient } from '@slack/web-api';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import os from 'os';
+import { Parser } from 'json2csv';
 dotenv.config();
 export class SlackService {
     client: WebClient;
@@ -24,6 +27,11 @@ export class SlackService {
         this.defaultChannel = defaultChannel;
     }
 
+    randStr(length: number) {
+        if (length > 128) throw new Error(`max length supported is 128`);
+        return require("crypto").randomBytes(64).toString("hex").slice(0,length);
+    }
+
     async sendMessage(message: string, channelId?: string): Promise<void> {
 
         const logPrefix = '::SlackService--sendMessage::';
@@ -39,6 +47,78 @@ export class SlackService {
             if(this.debug) console.log(`${logPrefix}error: ${err.message}`);
         }
 
+    }
+
+    async sendCsv(headers: string[], rows: Record<string, string>[], channelId?: string): Promise<void> {
+
+        const logPrefix = '::service--SlackService--sendCsvContent::';
+        const channel = channelId || this.defaultChannel;
+
+        const tempDir = os.tmpdir(), fileName = this.randStr(10);
+        const tempFilePath = `${tempDir}/${fileName}.csv`;
+        
+        try {
+
+            const csv = headers.join(',') + '\n' + rows.map(row =>
+                headers.map(header => `"${String(row[header]).replace(/"/g, "")}"`).join(','),
+            ).join('\n');
+
+            console.log('csv:',csv);
+
+            fs.writeFileSync(tempFilePath, csv);
+            console.log(`file written to temp directory: ${tempFilePath}`);
+
+            await this.client.files.uploadV2({
+                channel_id: channel,
+                file: fs.createReadStream(tempFilePath),
+                filename: tempFilePath.split('/').pop(),
+                title: fileName,
+            });
+
+            console.log(`${logPrefix} Uploaded CSV to Slack channel ${channel}`);
+
+        } catch (err: any) {
+            console.log(`${logPrefix} error: ${err.message}`);
+            throw err;
+
+        } finally{
+            if(fs.existsSync(tempFilePath)) {
+                fs.unlinkSync(tempFilePath);
+            }
+        }
+    }
+
+    async sendCsvFromObjectArray(objs: any[], channelId?: string): Promise<void> {
+
+        const logPrefix = '::service--SlackService--sendCsvFromObject::';
+
+        const tempDir = os.tmpdir(), fileName = this.randStr(10);
+        const tempFilePath = `${tempDir}/${fileName}.csv`;
+
+        try {
+            const parser = new Parser();
+            const csvStr = parser.parse(objs);
+            const channel = channelId || this.defaultChannel;
+            
+            fs.writeFileSync(tempFilePath, csvStr);
+            if (this.debug) console.log(`file written to temp directory: ${tempFilePath}`);
+
+            await this.client.files.uploadV2({
+                channel_id: channel,
+                file: fs.createReadStream(tempFilePath),
+                filename: tempFilePath.split('/').pop(),
+                title: fileName,
+            });
+
+            if (this.debug) console.log(`${logPrefix} Uploaded CSV to Slack channel ${channel}`);
+        } catch (err: any) {
+            console.log(`${logPrefix} error: ${err.message}`);
+            throw err;
+        } finally {
+            if (fs.existsSync(tempFilePath)) {
+                fs.unlinkSync(tempFilePath);
+            }
+        }
     }
     
 }
